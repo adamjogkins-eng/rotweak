6f5dd1addff9cd014122a33d51d469191038d2d4
adamjogkins-eng <adamjogkins@gmail.com>
2026-01-16 20:21:56 +0000

    Refactor iOSStrapMenu initialization and UI setup

--- iOSStrap.mm
+++ iOSStrap.mm
@@ -9,24 +9,13 @@
 
 @implementation iOSStrapMenu
 
-// Use the constructor to inject the menu into the window
+// This automatically starts the menu when the dylib loads
 __attribute__((constructor)) static void init() {
-    dispatch_after(dispatch_time(DISPATCH_TIME_NOW, (int64_t)(5 * NSEC_PER_SEC)), dispatch_get_main_queue(), ^{
-        UIWindow *window = nil;
-        if (@available(iOS 13.0, *)) {
-            for (UIWindowScene* scene in [UIApplication sharedApplication].connectedScenes) {
-                if (scene.activationState == UISceneActivationStateForegroundActive) {
-                    window = scene.windows.firstObject;
-                    break;
-                }
-            }
-        } else {
-            window = [UIApplication sharedApplication].keyWindow;
-        }
-
-        if (window) {
-            iOSStrapMenu *menu = [[iOSStrapMenu alloc] initWithFrame:window.bounds];
-            [window addSubview:menu];
+    dispatch_after(dispatch_time(DISPATCH_TIME_NOW, (int64_t)(3 * NSEC_PER_SEC)), dispatch_get_main_queue(), ^{
+        UIWindow *win = [UIApplication sharedApplication].keyWindow;
+        if (win) {
+            iOSStrapMenu *menu = [[iOSStrapMenu alloc] initWithFrame:win.bounds];
+            [win addSubview:menu];
         }
     });
 }
@@ -41,84 +30,64 @@ __attribute__((constructor)) static void init() {
 }
 
 - (void)setupUI {
-    // Draggable Floating Button
+    // 1. Draggable Toggle Dot
     self.dot = [UIButton buttonWithType:UIButtonTypeCustom];
-    self.dot.frame = CGRectMake(50, 150, 60, 60);
-    self.dot.backgroundColor = [UIColor colorWithRed:0.1 green:0.1 blue:0.1 alpha:0.9];
-    self.dot.layer.cornerRadius = 15; // Square-ish like the logo
-    self.dot.layer.borderWidth = 1.5;
+    self.dot.frame = CGRectMake(40, 100, 50, 50);
+    self.dot.backgroundColor = [UIColor blackColor];
+    self.dot.layer.cornerRadius = 10;
     self.dot.layer.borderColor = [UIColor whiteColor].CGColor;
+    self.dot.layer.borderWidth = 2;
     [self.dot setTitle:@"iOS" forState:UIControlStateNormal];
     [self.dot addTarget:self action:@selector(toggle) forControlEvents:UIControlEventTouchUpInside];
     
-    UIPanGestureRecognizer *pan = [[UIPanGestureRecognizer alloc] initWithTarget:self action:@selector(handlePan:)];
+    UIPanGestureRecognizer *pan = [[UIPanGestureRecognizer alloc] initWithTarget:self action:@selector(pan:)];
     [self.dot addGestureRecognizer:pan];
     [self addSubview:self.dot];
 
-    // Main Menu Container
-    self.container = [[UIView alloc] initWithFrame:CGRectMake(0, 0, 300, 400)];
+    // 2. Main Scrollable Container
+    self.container = [[UIView alloc] initWithFrame:CGRectMake(0, 0, 280, 380)];
     self.container.center = self.center;
-    self.container.backgroundColor = [UIColor colorWithWhite:0.05 alpha:0.98];
-    self.container.layer.cornerRadius = 12;
+    self.container.backgroundColor = [UIColor colorWithWhite:0.1 alpha:0.95];
+    self.container.layer.cornerRadius = 15;
     self.container.hidden = YES;
     self.container.userInteractionEnabled = YES;
     [self addSubview:self.container];
 
-    // Scrollable List
-    self.scroll = [[UIScrollView alloc] initWithFrame:CGRectMake(0, 40, 300, 360)];
-    self.scroll.contentSize = CGSizeMake(300, 500); 
+    self.scroll = [[UIScrollView alloc] initWithFrame:CGRectMake(0, 40, 280, 340)];
+    self.scroll.contentSize = CGSizeMake(280, 600); // Ensures scrolling works
     [self.container addSubview:self.scroll];
 
-    // Speed Button
-    UIButton *speed = [UIButton buttonWithType:UIButtonTypeSystem];
-    speed.frame = CGRectMake(20, 20, 260, 45);
-    [speed setTitle:@"WalkSpeed (200)" forState:UIControlStateNormal];
-    [speed setBackgroundColor:[UIColor colorWithWhite:0.2 alpha:1]];
-    [speed setTitleColor:[UIColor whiteColor] forState:UIControlStateNormal];
-    [self.scroll addSubview:speed];
-
-    // TP Menu Button
+    // 3. Teleport Sub-Menu Button
     UIButton *tpBtn = [UIButton buttonWithType:UIButtonTypeSystem];
-    tpBtn.frame = CGRectMake(20, 75, 260, 45);
+    tpBtn.frame = CGRectMake(10, 20, 260, 45);
     [tpBtn setTitle:@"Teleport to Player >" forState:UIControlStateNormal];
-    [tpBtn setBackgroundColor:[UIColor colorWithWhite:0.2 alpha:1]];
     [tpBtn setTitleColor:[UIColor cyanColor] forState:UIControlStateNormal];
-    [tpBtn addTarget:self action:@selector(openTeleport) forControlEvents:UIControlEventTouchUpInside];
+    [tpBtn addTarget:self action:@selector(showTP) forControlEvents:UIControlEventTouchUpInside];
     [self.scroll addSubview:tpBtn];
 }
 
-- (void)toggle { self.container.hidden = !self.container.hidden; }
+- (void)showTP {
+    self.tpMenu = [[UIView alloc] initWithFrame:self.container.bounds];
+    self.tpMenu.backgroundColor = [UIColor blackColor];
+    
+    UIButton *back = [UIButton buttonWithType:UIButtonTypeSystem];
+    back.frame = CGRectMake(10, 10, 60, 30);
+    [back setTitle:@"Back" forState:UIControlStateNormal];
+    [back addTarget:self action:@selector(hideTP) forControlEvents:UIControlEventTouchUpInside];
+    [self.tpMenu addSubview:back];
 
-- (void)handlePan:(UIPanGestureRecognizer *)p {
+    UIScrollView *plist = [[UIScrollView alloc] initWithFrame:CGRectMake(0, 50, 280, 330)];
+    // Add player buttons here dynamically
+    [self.tpMenu addSubview:plist];
+    [self.container addSubview:self.tpMenu];
+}
+
+- (void)toggle { self.container.hidden = !self.container.hidden; }
+- (void)hideTP { [self.tpMenu removeFromSuperview]; }
+- (void)pan:(UIPanGestureRecognizer *)p {
     CGPoint t = [p translationInView:self];
     self.dot.center = CGPointMake(self.dot.center.x + t.x, self.dot.center.y + t.y);
     [p setTranslation:CGPointZero inView:self];
 }
-
-- (void)openTeleport {
-    self.tpMenu = [[UIView alloc] initWithFrame:self.container.bounds];
-    self.tpMenu.backgroundColor = [UIColor blackColor];
-    self.tpMenu.layer.cornerRadius = 12;
-    
-    UIButton *back = [UIButton buttonWithType:UIButtonTypeSystem];
-    back.frame = CGRectMake(10, 10, 60, 30);
-    [back setTitle:@"< Back" forState:UIControlStateNormal];
-    [back addTarget:self action:@selector(closeTeleport) forControlEvents:UIControlEventTouchUpInside];
-    [self.tpMenu addSubview:back];
-
-    UIScrollView *pl = [[UIScrollView alloc] initWithFrame:CGRectMake(0, 50, 300, 350)];
-    NSArray *players = @[@"Player1", @"Player2", @"Admin"]; // Static list for stability
-    for(int i=0; i<players.count; i++) {
-        UIButton *b = [UIButton buttonWithType:UIButtonTypeSystem];
-        b.frame = CGRectMake(10, i*45, 280, 40);
-        [b setTitle:players[i] forState:UIControlStateNormal];
-        [pl addSubview:b];
-    }
-    pl.contentSize = CGSizeMake(300, players.count * 45);
-    [self.tpMenu addSubview:pl];
-    [self.container addSubview:self.tpMenu];
-}
-
-- (void)closeTeleport { [self.tpMenu removeFromSuperview]; }
 
 @end
